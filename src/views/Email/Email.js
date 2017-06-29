64c6d6e70e2903a2bc4adc7c42a934351e4f1cca
Shane Lian <wlian@thehonestcompany.com>
2017-06-28 22:33:48 -0700

    Update gitigonore

---
+++ src/views/Email/Email.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Email extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        Hello World from Email
+      </div>
+    )
+  }
+}
+
+export default Email;
